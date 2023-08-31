2bee3c9aabaafef69bc009f24223cfd7928aabfb
Juanjo Asensio García <asengar2009@gmail.com>
2023-08-31 22:03:30 +0200

    Start using React Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { React, useState } from "react";
-
 import { useTranslation } from "react-i18next";
 
 export default function App() {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
-import "./i18n"
-
+import "./i18n";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
