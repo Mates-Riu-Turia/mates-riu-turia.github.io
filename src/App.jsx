ecdc028ac6316ad81f9dfc74fa0149ceead50fac
Juanjo Asensio García <asengar2009@gmail.com>
2023-08-31 21:33:16 +0200

    Display the page title

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { React, useState } from "react";
+
+import { useTranslation } from "react-i18next";
+
+export default function App() {
+    const {t, i18n} = useTranslation();
+
+    // Set the web page title
+    document.title = t("title");
+}
