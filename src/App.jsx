6de0a5e5c48e1740ae81653342c28b4c7b117c66
Juanjo Asensio García <asengar2009@gmail.com>
2023-09-01 02:22:49 +0200

    Add basic UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import { React, useState } from "react";
 import { useTranslation } from "react-i18next";
+import { Nav, Footer, Logo } from "./components/basicUI";
 
 export default function App() {
-    const {t, i18n} = useTranslation();
+    const { t, i18n } = useTranslation();
 
     // Set the web page title
     document.title = t("title");
+
+    const changeLanguage = (lng) => {
+        i18n.changeLanguage(lng);
+    };
+
+    return (
+        <>
+            <Nav t={t} changeLanguage={changeLanguage} />
+            <Footer t={t} />
+        </>
+    );
 }
